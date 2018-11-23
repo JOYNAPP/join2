cda5ef5d8f9378aaafb82e8d707687c7921168d8
Uma Huggins <umahuggins@gmail.com>
2018-11-23 13:03:52 -0500

    seed associations

--- script/seed.js
+++ script/seed.js
@@ -4,116 +4,64 @@ const db = require('../server/db')
 const { User, Contract} = require('../server/db/models')
 const { green, red } = require('chalk')
 
-const contracts = [{
-  eventId: 56789,
-  fulfilled: false
-},{
-  eventId: 56790,
-  fulfilled: false
-},
-{
-  eventId: 56791
-}]
 
-const users = [{
-  name: 'Cody',
-  email: 'cody@email.com',
-  password: '123',
-}, {
-  name: 'Maggie',
-  email: 'maggie@email.com',
-  password: '123',
-}]
+const seed = async () => {
+  await db.sync({force: true})
+
+  const cody = await User.create({
+    name: 'Cody',
+    email: 'cody@email.com',
+    password: '123'
+  })
+
+  const maggie = await User.create({
+    name: 'Maggie',
+    email: 'maggie@email.com',
+    password: '123'
+  })
+
+  const maria = await User.create({
+    name: 'Maria',
+    email: 'maria@email.com',
+    password: '123'
+  })
+
+  const liv = await User.create({
+    name: 'Liv',
+    email: 'liv@email.com',
+    password: '123'
+  })
 
 
-const seed = () =>
-  Promise.all(contracts.map(elem => Contract.create(elem))
-  )
-    .then(() =>
-      Promise.all(users.map(elem => User.create(elem))
-      )
-    )
 
-const main = () => {
-  console.log('Syncing the db...');
-  db.sync({ force: true })
-    .then(() => {
-      console.log(green('Seeding the database...'));
-      return seed();
-    })
-    .catch(err => {
-      console.error(red('Oh noes! Something went wrong!'))
-      console.error(err);
-    })
-    .then(() => {
-      db.close();
-      console.log(green('Seeded Successfully!!'));
-      return null;
-    })
+  const beyonce = await Contract.create({
+    eventId: 56790,
+    fulfilled: false
+  })
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded ${contracts.length} contracts`)
+  const sting = await Contract.create({
+    eventId: 56790,
+    fulfilled: false
+  })
+
+  const kanye = await Contract.create({
+    eventId: 56791
+  })
+
+
+  await Promise.all([
+    beyonce.setUsers(maggie),
+    cody.setContracts(sting),
+    maggie.setContracts(kanye)
+  ])
+
+  console.log(green('Seeding success!'))
+  db.close()
 }
-main();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////////////////////////////////////////////////
-
-// 'use strict'
-
-// const db = require('../server/db')
-// const {User} = require('../server/db/models')
-
-// async function seed() {
-//   await db.sync({force: true})
-//   console.log('db synced!')
-
-//   const users = await Promise.all([
-//     User.create({email: 'cody@email.com', password: '123'}),
-//     User.create({email: 'murphy@email.com', password: '123'})
-//   ])
-
-//   console.log(`seeded ${users.length} users`)
-//   console.log(`seeded successfully`)
-// }
-
-// // We've separated the `seed` function from the `runSeed` function.
-// // This way we can isolate the error handling and exit trapping.
-// // The `seed` function is concerned only with modifying the database.
-// async function runSeed() {
-//   console.log('seeding...')
-//   try {
-//     await seed()
-//   } catch (err) {
-//     console.error(err)
-//     process.exitCode = 1
-//   } finally {
-//     console.log('closing db connection')
-//     await db.close()
-//     console.log('db connection closed')
-//   }
-// }
-
-// // Execute the `seed` function, IF we ran this module directly (`node seed`).
-// // `Async` functions always return a promise, so we can use `catch` to handle
-// // any errors that might occur inside of `seed`.
-// if (module === require.main) {
-//   runSeed()
-// }
-
-// // we export the seed function for testing purposes (see `./seed.spec.js`)
-// module.exports = seed
+seed()
+  .catch(err => {
+    console.error(red('Something went wrong'))
+    console.error(err)
+    db.close()
+  })
