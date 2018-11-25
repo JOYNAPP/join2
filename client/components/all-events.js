a48848cfe6c55b747a99284e6354a104d59468a9
Maggie Walker <mags21walker@gmail.com>
2018-11-25 16:48:04 -0500

    Feature Added: AllEvent Page Connected to Single Event

--- client/components/all-events.js
+++ client/components/all-events.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react'
+import history from '../history'
 
-export default class allEvent extends Component {
+export default class allEvents extends Component {
   render() {
     return (
       <div>
         <h2> All Events:</h2>
+        <h3>Drake</h3>
+        <img onClick={() => history.push('/event')}src="https://hollywoodstreetking.com/wordpress/wp-content/uploads/2017/03/drake-fans-boycott-concert.jpg" />
       </div>
     )
   }
