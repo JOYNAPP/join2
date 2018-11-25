387d6e744031e7e8dd54de14f05bfee80cb3ee4c
Liv Marx <oehrenmarks@gmail.com>
2018-11-25 13:42:38 -0500

    feat(allEvents): install eventbrite lib

--- client/components/all-events.js
+++ client/components/all-events.js
@@ -1,6 +1,23 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+import eventbrite from 'eventbrite'
+
+const sdk = eventbrite({token: 'AF36NVFKHSLG27TQBBWF'})
 
 export default class allEvent extends Component {
+  constructor() {
+    super()
+    this.state = {
+      events: []
+    }
+  }
+
+  componentDidMount() {
+    sdk.request('/users/me').then(res => {
+      console.log(res)
+    })
+  }
+
   render() {
     return (
       <div>
