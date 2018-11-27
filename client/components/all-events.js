e9334f596289e157f6b3b18768b37f8e1b21ebab
Liv Marx <oehrenmarks@gmail.com>
2018-11-27 15:13:54 -0500

    feat(all-events): make axios req to EB api

--- client/components/all-events.js
+++ client/components/all-events.js
@@ -13,30 +13,43 @@ export default class allEvents extends Component {
   }
 
   async componentDidMount() {
-    const res = await axios.get('/api/events')
-    const events = res.data
-    console.table('dringus', events)
+    const res = await axios.get(
+      `https://www.eventbriteapi.com/v3/events/search/?q=$drake&token=AF36NVFKHSLG27TQBBWF`
+    )
+    const events = res.data.events
+    console.log('dringus', events)
     this.setState({events: events})
     console.table(events)
   }
 
   render() {
+    const events = this.state.events || []
+    console.log('THESE EVENTS:', this.state.events)
     return (
       <div>
-        <h2> All Events:</h2>
-        {this.state.events.map(event => {
-          return (
-            <div className="event-list" key={event.id}>
-              <h3>
-                <Link to={`/events/${event.id}`}>{event.name.text}</Link>
-              </h3>
-              <div>Date: {event.start.local}</div>
-              <img src={event.logo} onClick={() => history.push('/event')} />
-              <br />
-              <br />
-            </div>
-          )
-        })}
+        {this.state.events &&
+          this.state.events.map(event => {
+            return (
+              <div className="event-list" key={event.id}>
+                <h3>
+                  <Link to={`/events/${event.id}`}>{event.name.text}</Link>
+                </h3>
+                <div>Date: {event.start.local}</div>
+                <p>
+                  {event.description.text.length > 399
+                    ? event.description.text.slice(0, 400) + '...'
+                    : event.description.text}
+                </p>
+                {event.logo ? (
+                  <img src={event.logo.url} />
+                ) : (
+                  <img src="https://allmods.net/wp-content/uploads/2018/08/no-image-available.png" />
+                )}
+                <br />
+                <br />
+              </div>
+            )
+          })}
       </div>
     )
   }
